e1f8fefa4a92cfd60fc540c0538b8c221121d924
Tyler Barabas <tylerbarabas@gmail.com>
2017-11-22 23:44:23 -0800

    import createjs

--- src/core-components/AudioPlayer.js
+++ src/core-components/AudioPlayer.js
@@ -1,7 +1,10 @@
+import 'latest-createjs';
+const createjs = window.createjs;
 
 export default class Stage {
 
     constructor(){
+        createjs.Sound.registerPlugins([createjs.WebAudioPlugin, createjs.FlashAudioPlugin]);	
     }
 
     init(){
