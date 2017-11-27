68abeef0064ff7282fa2967a0961f78e360273cd
Tyler Barabas <tylerb@d2.com>
2017-11-26 23:37:32 -0800

    test out audio player. it works!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Stage from 'core-components/Stage';
-//import AudioPlayer from 'core-components/AudioPlayer';
+import AudioPlayer from 'core-components/AudioPlayer';
 import './index.css';
+import TestAudio from './snd/test.mp3';
 
 const ContentStage = new Stage();
 ContentStage.init();
 
-//const AP = new AudioPlayer();
-//console.log('AP',AP);
+const AP = new AudioPlayer();
+AP.addEvent('songLoaded', AP.play.bind(AP,'test'));
+AP.loadFile(TestAudio, 'test');
